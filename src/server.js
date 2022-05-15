b85dbcb2c863fdc205cb0a6d3f072a337454babf
Jorge MC <angelus.tux@gmail.com>
2022-05-15 08:41:29 -0500

    modificando server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
+const app = require('./app');
+
 const port = 3000
 
 app.get('/',(req,res) => {
     res.send('OIL FISH')
 })
 
-app.listen(port,()=>{
+/*app.listen(port,()=>{
     console.log(`escucha por el puerto ${port}`)
-})
+})*/
+
+async function main(){
+    await app.listen(3000);
+    console.log("Escucha por el puerto 3000");
+}
+
+main();
